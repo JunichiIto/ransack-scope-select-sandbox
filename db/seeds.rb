be878bc4c8e197a4b54294e3c01a7009b1a54114
Junichi Ito <jit@sonicgarden.jp>
2022-05-03 14:50:37 +0900

    Tweak seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ Student.destroy_all
 
 Student.create!(name: 'Alice')
 bob = Student.create!(name: 'Bob')
-bob.exams.create!(subject: 'English', score: 55)
-bob.exams.create!(subject: 'math', score: 70)
-bob.exams.create!(subject: 'Japanese', score: 40)
+bob.exams.create!(subject: 'English', score: 60)
+bob.exams.create!(subject: 'math', score: 57)
+bob.exams.create!(subject: 'Japanese', score: 48)
 
 carol = Student.create!(name: 'Carol')
 carol.exams.create!(subject: 'English', score: 77)
